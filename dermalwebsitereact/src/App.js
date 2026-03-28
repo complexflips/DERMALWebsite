4d4f1131d972b35bb226031314998c6c59116099
complexflips <pjf0831@yahoo.com>
2026-03-28 08:48:37 -0500

    modified:   dermalwebsitereact/src/App.js deleted:    dermalwebsitereact/src/DisplayTasks.js 	deleted:    dermalwebsitereact/src/data.json

--- dermalwebsitereact/src/App.js
+++ dermalwebsitereact/src/App.js
@@ -2,7 +2,7 @@ import React, {useState,useEffect} from 'react';
 import axios from 'axios';
 import './App.css';
 import DERMALForm from './components/DERMALForm';
-import DisplayTasks from './DisplayTasks';
+
 
 function App() {
     const [tasks,setTasks]=useState([]);
